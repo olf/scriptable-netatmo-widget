a979f095e0a281215b400ce4e39184a7fd345765
Olaf Peters <olf@magicolf.de>
2020-10-25 21:58:43 +0100

    Add script code

---
+++ Netatmo.js
@@ -0,0 +1,128 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: umbrella;
+function createWidget(moduleName, moduleData) {
+  let widget = new ListWidget()
+
+  widget.backgroundColor = Color.black()
+
+  let title = widget.addText(moduleName)
+  title.font = Font.boldSystemFont(18)
+  title.minimumScaleFactor = 0.8
+  title.lineLimit = 2
+  title.textColor = Color.white()
+
+  widget.addSpacer(8)
+
+  // Temperature
+
+  let temperature = widget.addText("" + moduleData.Temperature + "°C")
+  temperature.font = Font.regularSystemFont(24)
+  temperature.textColor = Color.white()
+  temperature.centerAlignText()
+
+  widget.addSpacer(8)
+
+  // CO2
+
+  if ("CO2" in moduleData) {
+    co2value = moduleData.CO2
+
+    let co2 = widget.addText("CO₂: " + co2value)
+    co2.font = Font.regularSystemFont(18)
+    co2.centerAlignText()
+
+    co2.textColor = Color.green()
+    if (co2value >= 1000) {
+      co2.textColor = Color.orange()
+    } else if (co2value >= 1500) {
+      co2.textColor = Color.red()
+    }
+
+    widget.addSpacer(8)
+  }
+
+  // Timestamp
+
+  const date = new Date(moduleData.time_utc * 1000)
+  let dateFormatter = new DateFormatter()
+  dateFormatter.useShortDateStyle()
+  dateFormatter.useShortTimeStyle()
+  let strDate = dateFormatter.string(date)
+
+  let footer = widget.addText(strDate)
+  footer.minimumScaleFactor = 0.5
+  footer.lineLimit = 1
+  footer.textColor = Color.lightGray()
+  footer.centerAlignText()
+
+  return widget
+}
+
+async function authenticate(app_id, app_secret, username, password) {
+  let req = new Request("https://api.netatmo.net/oauth2/token")
+
+  req.method = "POST"
+
+  req.addParameterToMultipart("grant_type", "password")
+  req.addParameterToMultipart("client_id", app_id)
+  req.addParameterToMultipart("client_secret", app_secret)
+  req.addParameterToMultipart("username", username)
+  req.addParameterToMultipart("password", password)
+  req.addParameterToMultipart("scope", "read_station")
+
+  let response = await req.loadJSON()
+  return response.access_token
+}
+
+async function getData(token) {
+  let req = new Request(
+    "https://api.netatmo.net/api/getstationsdata?access_token=" +
+      encodeURI(token)
+  )
+
+  let response = await req.loadJSON()
+  return response.body
+}
+
+function getModuleData(moduleName, data) {
+  device = data.devices[0]
+
+  if (device.module_name === moduleName) {
+    return device.dashboard_data
+  }
+
+  modules = device.modules.filter((item) => item.module_name === moduleName)
+
+  if (modules.length === 0) {
+    return {}
+  } else {
+    return modules[0].dashboard_data
+  }
+}
+
+let netatmoConfig = importModule("Netatmo.config")
+
+let token = await authenticate(
+  netatmoConfig.app_id,
+  netatmoConfig.app_secret,
+  netatmoConfig.username,
+  netatmoConfig.password
+)
+
+let data = await getData(token)
+
+if (config.runsInApp) {
+  // For in-app testing
+  const module_name = "Indoor"
+
+  let widget = createWidget(module_name, getModuleData(module_name, data))
+
+  widget.presentSmall()
+} else {
+  let parameter = args.widgetParameter
+
+  let widget = createWidget(parameter, getModuleData(parameter, data))
+
+  Script.setWidget(widget)
+}
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Scriptable-netatmo-widget
 
-Widget for the Scriptable iOS app to display data from Netatmo personal weather stations
+Widget for the [Scriptable][app] iOS app to display data from [Netatmo][netatmo] personal weather stations.
+
+## Installation
+
+Pre-requisite: You need to [register a new application][newapp] in the [Netatmo Developer Portal][devportal] to get readings from your personal weather station.
+
+* rename the `Netatmo.config-template` file to `Netatmo.config` and fill in your details from the app registration and your login/password
+* copy both the Netatmo.config and Netatmo.js files to the scriptable folder in your iCloud Drive
+* add a new Scriptable widget in iOS, specify the name of the Netatmo module you want to display in the widget "Parameter" setting
+
+## Preview
+
+![Preview Image][preview]
+
+[app]: https://scriptable.app
+[devportal]: https://dev.netatmo.com/
+[netatmo]: https://www.netatmo.com/weather
+[newapp]: https://dev.netatmo.com/apps/createanapp
+[preview]: preview.png
